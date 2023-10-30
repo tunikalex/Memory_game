1faee876b5df3cbf260a826c8c0b701337ed5412
aleksandr <tunikalex@yandex.by>
2023-10-30 22:34:36 +0100

    работает раскладка карт

--- main.js
+++ main.js
@@ -29,11 +29,117 @@ function shuffle(arr) {
 
 // Этап 3. Используйте две созданные функции для создания массива перемешанными номерами. На основе этого массива вы можете создать DOM-элементы карточек. У каждой карточки будет свой номер из массива произвольных чисел. Вы также можете создать для этого специальную функцию. count - количество пар.
 
-function startGame(count) {
-  let originArr = createNumbersArray(count = count);
-  console.log(`Массив чисел: `, originArr);
-  const shuffleArr = shuffle(arr = originArr);
-  console.log(`Перемешанный массив: ${shuffleArr}`);
+function howMuch(callback) {
+  document.body.querySelector('input').addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      const inputValue = Number(this.value);
+
+      if (isNaN(inputValue)) {
+        alert("The number cannot be text. \nTry entering it again.");
+      } else if (inputValue < 6) {
+        alert("The number cannot be less than 6. \nTry entering another number.");
+      } else if (inputValue > 80) {
+        alert("The number cannot be more than 80. \nTry entering another number.");
+      } else if (inputValue % 2 !== 0) {
+        alert("For this game the number of cards must be even");
+      } else {
+        e.preventDefault();
+        const quantityNum = inputValue / 2;
+        this.value = '';
+        callback(quantityNum);
+      }
+    }
+  });
 }
 
-startGame()
+
+function createInput() {
+  const main = document.createElement('main');
+  const title = document.createElement('h1')
+  const form = document.createElement('form')
+  const input = document.createElement('input');
+
+  title.classList.add('title')
+  main.classList.add('container');
+  main.id = 'main';
+  form.classList.add('form')
+  input.classList.add('form__input');
+  input.id = 'input';
+  // input.oninput = 'test = this.value'
+
+  title.textContent = 'Memory Game'
+  input.placeholder = 'Enter quantity kards'
+
+  document.body.appendChild(main);
+  main.appendChild(title);
+  main.appendChild(form);
+  form.appendChild(input);
+}
+
+
+function crateGamesTable(arr) {
+  const list = document.querySelector('ul');
+  let ind = 0;
+
+  if (list === null) {
+    let list = document.createElement('ul');
+    list.classList.add('list');
+    document.querySelector('#main').appendChild(list);
+
+    for (let iNum of arr) {
+      const item = document.createElement('il');
+      const shirtUp = document.createElement('button');
+      item.textContent = iNum;
+      shirtUp.classList.add('list__shirt');
+      item.classList.add('list__item');
+      item.id = ind;
+      list.append(item);
+      item.append(shirtUp);
+      ind++;
+    }
+
+  } else {
+    list.innerHTML = '';
+    for (let iNum of arr) {
+      const item = document.createElement('il');
+      const shirtUp = document.createElement('button');
+      item.textContent = iNum;
+      shirtUp.classList.add('list__shirt');
+      item.classList.add('list__item');
+      item.id = ind;
+      list.append(item);
+      item.append(shirtUp);
+      ind++;
+    }
+  }
+};
+
+
+function playGame() {
+  const handleClick = (event) => {
+    console.log(`event.target= ${event.target}`)
+  }
+  const cards = document.querySelectorAll('.list__shirt');
+  console.log(`cards= ${cards}`);
+  for (i of cards) {
+  console.log(`i = ${i}`);
+  }
+  cards.forEach(card => {
+    console.log(`card= ${card}`);
+    card.addEventListener('click', (event) => {
+      console.log(`event.target= ${event.target}`)
+    })
+  })
+  };
+
+function startGame() {
+  createInput();
+  howMuch(quantityNums => {
+    let originArr = createNumbersArray(count = quantityNums);
+    const shuffleArr = shuffle(arr = originArr);
+    crateGamesTable(arr = shuffleArr);
+  })
+}
+
+startGame();
+playGame()
